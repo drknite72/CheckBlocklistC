f1006a31fbe93b815033bad2f9a31a43dbc00f8c
James Hopwood <JHopwood@RoomsToGo.com>
2019-10-07 09:53:01 -0400

    cleanup

--- CheckBlocklistC/Program.cs
+++ CheckBlocklistC/Program.cs
@@ -20,18 +20,6 @@ namespace CheckBlocklistC
             request.Method = "HEAD";
             var checkresponce = String.Empty;
 
-            //try
-            //{
-            //    var response = (HttpWebResponse)request.GetResponse();
-            //    checkresponce = response.ToString();
-            //}
-            //catch (Exception e)
-            //{
-
-            //    //checkresponce = "Exception: " + e.Message.ToString();
-            //    checkresponce = ((HttpWebResponse)e.Response).StatusCode;
-            //}
-
             HttpWebResponse response = null;
             HttpStatusCode statusCode;
             var sResponse = String.Empty;
@@ -42,33 +30,18 @@ namespace CheckBlocklistC
             catch (WebException we)
             {
                 response = (HttpWebResponse)we.Response;
-
-
             }
 
             if (response == null)
             {
-                //checkresponce = "no responce from site";
                 checkresponce = "Response Code: 408 - RequestTimeout";
             }
             else
             {
                 statusCode = response.StatusCode;
-                //Stream dataStream = response.GetResponseStream();
-                //StreamReader reader = new StreamReader(dataStream);
-                //sResponse = reader.ReadToEnd();
-                //Console.WriteLine(sResponse);
-                //Console.WriteLine("Response Code: " + (int)statusCode + " - " + statusCode.ToString());
-
                 checkresponce = "Response Code: " + (int)statusCode + " - " + statusCode.ToString();
             }
-
-
-            // status code...
-            //return response.StatusCode;
-
             return checkresponce;
-
         }
 
 
